be3f051bdb37297d49fdd24debc5d09bb9dc2ad5
João Carli <46573038+fl4m3x@users.noreply.github.com>
2020-02-19 19:45:53 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,11 @@ server.use(express.urlencoded({ extended: true}))
 
 const Pool = require('pg').Pool
 const db = new Pool({
-    user: '{user}',
-    password: '{password}',
-    host: '{localhost}',
-    port: {port},
-    database: 'doe'
+  user: 'postgres',
+  password: '0000',
+  host: 'localhost',
+  port: 5432,
+  database: 'doe'
 })
 
 const nunjucks = require("nunjucks")
